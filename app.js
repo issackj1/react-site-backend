7f3c3d5917d4237973bfc587944fd222639db096
issackj1 <issack.john1@ucalgary.ca>
2020-06-27 16:42:12 -0400

    misc

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const cors = require('cors');
+const axios = require('axios');
+const whitelist = ['http://localhost:3000/react-site#/', 'http://localhost:3000'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+};
 
-var app = express();
+const app = express();
 
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -18,7 +31,35 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.get('/api/v1/test', function (req, res) {
-    res.send('This is a test')
+    res
+        .status(200)
+        .send({
+            message: 'This is a test',
+        })
+});
+
+app.get('/api/v1/test1', function (req, res) {
+    console.log('hey one');
+    axios.get("https://www150.statcan.gc.ca/t1/wds/rest/getChangedSeriesList", {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'user_key': process.env.user_key
+        }
+    })
+        .then(
+            (result) => {
+                res
+                    .status(200)
+                    .send(result.data);
+            },
+            (error) => {
+                res
+                    .status(error.status)
+                    .send(error.data)
+            }
+        );
 });
 
 module.exports = app;
