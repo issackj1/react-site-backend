db07cc2dad9dcfb664474d4c4b2ccc3ad43b61c5
issackj1 <issack.john1@ucalgary.ca>
2020-06-28 15:04:53 -0400

    implemented routes

--- controller/StatisticsCanadaWDSController.js
+++ controller/StatisticsCanadaWDSController.js
@@ -31,21 +31,96 @@ module.exports.getCubeMetaData = (req, res, next) => {
 };
 
 module.exports.getDataFromVectorsAndLatestNPeriods = (req, res, next) => {
-    res
-        .status(200)
-        .send("getDataFromVectorsAndLatestNPeriods")
+    axios({
+        method: 'post',
+        url: 'https://statcan-web-data-service-statcan.api.canada.ca/v1/getDataFromVectorsAndLatestNPeriods',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'user_key': process.env.user_key
+        },
+        responseType: 'json',
+        data: [
+            {
+                vectorId: req.params.vectorId,
+                latestN: req.params.latestN,
+            }
+        ]
+    })
+        .then(
+            (result) => {
+                res
+                    .status(result.status)
+                    .send(result.data);
+            },
+            (error) => {
+                res
+                    .status(error.response.status)
+                    .send(error.response.data)
+            }
+        );
 
 };
 
 module.exports.getDataFromCubePidCoordAndLatestNPeriods = (req, res, next) => {
-    res
-        .status(200)
-        .send("getDataFromCubePidCoordAndLatestNPeriods")
+    axios({
+        method: 'post',
+        url: 'https://statcan-web-data-service-statcan.api.canada.ca/v1/getDataFromCubePidCoordAndLatestNPeriods',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'user_key': process.env.user_key
+        },
+        responseType: 'json',
+        data: [
+            {
+                productId: req.params.productId,
+                coordinate: req.params.coordinate,
+                latestN: req.params.latestN,
+            }
+        ]
+    })
+        .then(
+            (result) => {
+                res
+                    .status(result.status)
+                    .send(result.data);
+            },
+            (error) => {
+                res
+                    .status(error.response.status)
+                    .send(error.response.data)
+            }
+        );
 };
 
 module.exports.getChangedSeriesDataFromVector = (req, res, next) => {
-    res
-        .status(200)
-        .send("getDataFromVectorsAndLatestNPeriods")
+    axios({
+        method: 'post',
+        url: 'https://statcan-web-data-service-statcan.api.canada.ca/v1/getChangedSeriesDataFromVector',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'user_key': process.env.user_key
+        },
+        responseType: 'json',
+        data: [
+            {
+                vectorId: req.params.vectorId
+            }
+        ]
+    })
+        .then(
+            (result) => {
+                res
+                    .status(result.status)
+                    .send(result.data);
+            },
+            (error) => {
+                res
+                    .status(error.response.status)
+                    .send(error.response.data)
+            }
+        );
 };
 
