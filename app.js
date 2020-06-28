bdf8687d08a7fd22d1c75e5323f3d5a350c7958a
issackj1 <issack.john1@ucalgary.ca>
2020-06-27 23:03:57 -0400

    misc

--- app.js
+++ app.js
@@ -33,31 +33,37 @@ app.use('/users', usersRouter);
 app.get('/api/v1/test', function (req, res) {
     res
         .status(200)
-        .send({
+        .json({
             message: 'This is a test',
         })
 });
 
-app.get('/api/v1/test1', function (req, res) {
-    console.log('hey one');
-    axios.get("https://www150.statcan.gc.ca/t1/wds/rest/getChangedSeriesList", {
+app.get('/api/v1/getCubeMetaData/:id', function (req, res) {
+    axios({
+        method: 'post',
+        url: 'https://statcan-web-data-service-statcan.api.canada.ca/v1/getCubeMetadata',
         headers: {
-            'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json',
             'Accept': 'application/json',
             'user_key': process.env.user_key
-        }
+        },
+        responseType: 'json',
+        data: [
+                {
+                    productId: req.params.id,
+                }
+            ]
     })
         .then(
             (result) => {
                 res
-                    .status(200)
-                    .send(result.data);
+                    .status(result.status)
+                    .send(result.data[0]);
             },
             (error) => {
                 res
-                    .status(error.status)
-                    .send(error.data)
+                    .status(error.response.status)
+                    .send(error.response.data)
             }
         );
 });
