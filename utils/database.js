52203b7fdad18fcf316def1c17e7fc3d7803560b
issackj1 <issack.john1@ucalgary.ca>
2020-08-30 17:52:22 -0600

    removed unncessary alert

--- utils/database.js
+++ utils/database.js
@@ -4,4 +4,4 @@ const DB_NAME = process.env.DB_NAME;
 const USER_PWD = process.env.USER_PWD;
 const CLUSTER = process.env.CLUSTER;
 
-module.exports.DB_URI = PROTOCOL + DB_USER + ":" + USER_PWD + CLUSTER + DB_NAME;
+module.exports.DB_URI = PROTOCOL + DB_USER + ":" + USER_PWD + CLUSTER + DB_NAME + "?retryWrites=true&w=majority";
