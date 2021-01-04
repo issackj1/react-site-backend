cee48a95bda98061829b1fd3498da04aaf7c8ebc
issackj1 <issack.john1@ucalgary.ca>
2021-01-04 12:38:05 -0700

    minor refactoring

--- middleware/utility.js
+++ middleware/utility.js
@@ -1,15 +1,15 @@
 const cors = require('cors'),
-    express_session = require('express-session'),
-    MongoDBSession = require('connect-mongodb-session')(express_session);
+    session = require('express-session'),
+    MongoDBStore = require('connect-mongodb-session')(session);
 
 const database = require('../utils/database');
 
-module.exports.session = express_session({
+module.exports.session = session({
     secret: process.env.EXPRESS_SESSION_SECRET,
     resave: true,
     saveUninitialized: false,
     expires: new Date(Date.now() + (30 * 86400 * 1000)),
-    store: new MongoDBSession({
+    store: new MongoDBStore({
         uri: database.DB_URI,
         collection: "sessions"
     }),
